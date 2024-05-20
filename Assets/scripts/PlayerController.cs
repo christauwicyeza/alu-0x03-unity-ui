66a34513a2e89b731e15b1d907d2d5e5c0c87bae
My Name <c.uwicyeza@alustudent.com>
2024-05-20 19:46:59 +0300

    onTrigger

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // Added this line
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 50f;
     private Rigidbody rb;
     private int score = 0;
-    public int health = 5; 
+    public int health = 5;
+
     void Start()
-    { 
+    {
         rb = GetComponent<Rigidbody>();
         Debug.Log("game started");
     }
@@ -21,33 +21,34 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         float verticalMove = Input.GetAxisRaw("Vertical");
-        rb.AddForce(new Vector3(horizontalMove, 0.0f, verticalMove) * (speed * Time.deltaTime)); 
+        rb.AddForce(new Vector3(horizontalMove, 0.0f, verticalMove) * (speed * Time.deltaTime));
     }
 
-     void Update()
+    void Update()
     {
         if (health == 0)
         {
-            Debug.Log("Game Over!"); 
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
-            score++; 
+            score++;
             Debug.Log("Score: " + score);
             other.gameObject.SetActive(false);
         }
         else if (other.gameObject.CompareTag("Trap"))
         {
-            health--; 
+            health--;
             Debug.Log("Health: " + health);
         }
         else if (other.gameObject.CompareTag("Goal"))
         {
-            Debug.Log("You win!"); 
+            Debug.Log("You win!");
         }
-    }
     }
 }

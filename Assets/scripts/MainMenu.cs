17ea5d3aa7d2b7d43055fab1d078668cecfe466c
My Name <c.uwicyeza@alustudent.com>
2024-05-23 01:27:55 +0300

    color blind

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
      public void PlayMaze()
-    {
+    {    
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 255); // Orange
+            goalMat.color = Color.blue; // Blue
+        }
         SceneManager.LoadScene("maze");
     }
 
